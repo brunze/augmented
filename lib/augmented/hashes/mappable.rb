e0235af819a91d135e0f9845909dfac6ac323634
bsantos <santos.bt3@gmail.com>
2016-01-17 11:34:45 +0000

    Enhances map_values and map_keys with extra arguments.

--- lib/augmented/hashes/mappable.rb
+++ lib/augmented/hashes/mappable.rb
@@ -4,11 +4,11 @@ module Augmented
       refine Hash do
 
         def map_values
-          self.each_with_object({}){ |(key, value), result| result[key] = yield value }
+          self.each_with_object({}){ |(key, value), result| result[key] = yield value, key, self }
         end
 
         def map_keys
-          self.each_with_object({}){ |(key, value), result| result[yield key] = value }
+          self.each_with_object({}){ |(key, value), result| result[yield key, value, self] = value }
         end
 
       end
--- test/augmented/hashes/mappable_test.rb
+++ test/augmented/hashes/mappable_test.rb
@@ -10,12 +10,26 @@ describe Augmented::Hashes::Mappable do
       { aa: 11, bb: 22 }.map_values{ |i| i * 3 }.must_equal({ aa: 33, bb: 66 })
     end
 
+    it 'also provides the key and hash to the tranformer function as additional arguments' do
+      hash = { aa: 11, bb: 22 }
+      result = hash.map_values{ |i, key, h| [key, h.object_id] }
+
+      result.values.must_equal [[:aa, hash.object_id], [:bb, hash.object_id]]
+    end
+
   end
 
   describe '#map_keys' do
 
     it 'returns a new hash with the same values but transformed keys' do
       { aa: 11, bb: 22 }.map_keys{ |k| k.to_s[0] }.must_equal({ 'a' => 11, 'b' => 22 })
+    end
+
+    it 'also provides the value and hash to the tranformer function as additional arguments' do
+      hash = { aa: 11, bb: 22 }
+      result = hash.map_keys{ |k, value, h| [value, h.object_id] }
+
+      result.keys.must_equal [[11, hash.object_id], [22, hash.object_id]]
     end
 
   end
